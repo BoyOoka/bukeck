92d96ed35264b1aa20fa947329fe0f1637241cbc
waterkiss@126.com <8623116p>
2022-06-27 22:42:00 +0800

    maven 3.6.3

--- src/main/java/jedisShow.java
+++ src/main/java/jedisShow.java
@@ -2,7 +2,7 @@ import redis.clients.jedis.Jedis;
 
 public class jedisShow {
     public static void main(String[] args){
-        Jedis jedis = new Jedis("192.168.2.9", 6379);
+        Jedis jedis = new Jedis("localhost", 6379);
         String data = jedis.srandmember("proxy:proxy_info");
         System.out.println(data);
         jedis.close();
