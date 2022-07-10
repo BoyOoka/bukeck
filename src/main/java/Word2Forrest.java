895606d60bd0a2e8909710619afca5f8e6bcb2de
waterkiss@126.com <8623116p>
2022-07-10 18:07:20 +0800

    doc test demo

--- src/main/java/Word2Forrest.java
+++ src/main/java/Word2Forrest.java
@@ -1,7 +1,10 @@
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.hwpf.model.ListData;
+import org.apache.poi.hwpf.model.ListTables;
 import org.apache.poi.hwpf.model.StyleDescription;
 import org.apache.poi.hwpf.model.StyleSheet;
+import org.apache.poi.hwpf.usermodel.BorderCode;
 import org.apache.poi.hwpf.usermodel.CharacterRun;
 import org.apache.poi.hwpf.usermodel.Paragraph;
 import org.apache.poi.hwpf.usermodel.Range;
@@ -195,12 +198,35 @@ public class Word2Forrest {
 //             OutputStream out = new FileOutputStream("test.xml")) {
 //            new Word2Forrest(new HWPFDocument(is), out);
 //        }
-        File file = new File("2014.doc");
+        File file = new File("2.doc");
         FileInputStream in = new FileInputStream(file);
         HWPFDocument doc = new HWPFDocument (in);
-        String text = doc.getText().toString();
+//        String text = doc.getText().toString();
 //        WordExtractor extract = new WordExtractor(doc);
 //        String text = extract.getText();
-        System.out.println(text);
+//        System.out.println(text);
+        Range range = doc.getRange();
+        int numP = range.numParagraphs();
+        for (int i = 0; i < numP; ++i) {
+            //从每一段落中获取文字
+            Paragraph p = range.getParagraph(i);
+            if(p.isInTable()){
+                continue;
+            }
+            int Ilvl = p.getIlvl();
+            int Ilfo = p.getIlfo();
+            int p_run = p.numCharacterRuns();
+            int level = p.getLvl();  // 大纲等级
+            System.out.println("leve:" + level);
+            for(int j=0; j<p_run; ++j){
+                CharacterRun r_run = p.getCharacterRun(j);
+                String fontName = r_run.getFontName();
+                String r_text = r_run.text();
+                int fontSize = r_run.getFontSize();
+                boolean border = r_run.isBold();
+                System.out.println(fontSize + "-" +border);
+                System.out.println(Ilvl + ":" +Ilfo + "-" + fontName + ":" + r_text);
+            }
+        }
     }
 }
--- src/main/java/jedisShow.java
+++ src/main/java/jedisShow.java
@@ -1,10 +1,17 @@
 import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
+
 public class jedisShow {
     public static void main(String[] args){
-        Jedis jedis = new Jedis("localhost", 6379);
-        String data = jedis.srandmember("proxy:proxy_info");
-        System.out.println(data);
-        jedis.close();
+//        Jedis jedis = new Jedis("localhost", 6379);
+//        String data = jedis.srandmember("proxy:proxy_info");
+//        System.out.println(data);
+//        jedis.close();
+        Integer args1[]= {1,2,3,4,5};
+        for (Integer b: args1){
+            System.out.println(b);
+
+        }
     }
 }
